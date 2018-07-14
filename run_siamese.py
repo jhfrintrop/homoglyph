00aa1375d34e626bcb09184c26247691501d9ff9
Jan-Hendrik Frintrop <>
2018-07-14 12:44:55 +0200

    "Fix" fomatting
    - whitespaces
    - empty lines / linebreaks
    - leading 0 before dot

--- run_siamese.py
+++ run_siamese.py
@@ -17,12 +17,11 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Sequential, Model, model_from_json
 from keras import backend as K
 from keras.optimizers import RMSprop
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont
 from sklearn.metrics import roc_curve, auc, roc_auc_score
 
-isFast = True # If True, then it runs on a very small dataset (and results won't be that great)
+
+is_fast = True  # If True, then it runs on a very small dataset (and results won't be that great)
 
 dataset_type = 'process'
 #dataset_type = 'domain'
@@ -43,7 +42,8 @@ elif dataset_type == 'process':
     IMAGE_FILE = os.path.join(OUTPUT_DIR, 'process_roc_curve.png')
     OUTPUT_NAME = 'Process Spoofing'
 else:
-    raise Exception('Unknown dataset type: %s' % (dataset_type,))
+    raise Exception('Unknown dataset type: {}'.format(dataset_type))
+
 
 def generate_imgs(strings, font_location, font_size, image_size, text_location):
     font = ImageFont.truetype(font_location, font_size)
@@ -62,36 +62,40 @@ def generate_imgs(strings, font_location, font_size, image_size, text_location):
 
     return np.array(str_imgs, dtype=np.float32)
 
+
 def euclidean_distance(vects):
     x, y = vects
-    return K.sqrt(K.sum(K.square(x - y)+np.random.rand()*.0001, axis=1, keepdims=True))
+    return K.sqrt(K.sum(K.square(x - y) + np.random.rand() * 0.0001, axis=1, keepdims=True))
 
 
 def eucl_dist_output_shape(shapes):
     shape1, shape2 = shapes
-    return (shape1[0], 1)
+    return shape1[0], 1
+
 
 def contrastive_loss(y_true, y_pred):
-    '''Contrastive loss from Hadsell-et-al.'06
+    """Contrastive loss from Hadsell-et-al.'06
     http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-    '''
+    """
+
     margin = 1
     return K.mean(y_true * K.square(y_pred) + (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)), axis=-1, keepdims=False)
 
-def build_model(data_shape):                                                                                                                   
+
+def build_model(data_shape):
     model = Sequential()
 
     model.add(Convolution2D(128, 5, 5, input_shape=data_shape))
-    model.add(LeakyReLU(alpha=.1))                                                                                                                                                          
+    model.add(LeakyReLU(alpha=0.1))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Convolution2D(64, 3, 3))
-    model.add(LeakyReLU(alpha=.1))
+    model.add(LeakyReLU(alpha=0.1))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Flatten())                                                                                                                                                                 
+    model.add(Flatten())
     model.add(Dense(32))
 
     input_a = Input(shape=data_shape)
@@ -104,14 +108,16 @@ def build_model(data_shape):
 
     model = Model(input=[input_a, input_b], output=distance)
 
-    # # train
+    # train
     rms = RMSprop()
-    model.compile(loss=contrastive_loss, optimizer=rms) 
+    model.compile(loss=contrastive_loss, optimizer=rms)
 
     return model
 
+
 def initialize_encoder(self):
     """Initialize encoder for translating images to features."""
+
     # Set locations of models, weights, and feature parameters
     model_file = os.path.join(OUTPUT_DIR, dataset_type + '_cnn.json')
     weight_file = os.path.join(OUTPUT_DIR, dataset_type + '_cnn.h5')
@@ -139,24 +145,33 @@ if not os.path.isfile(OUTPUT_FILE):
     with open(INPUT_FILE) as f:
         data = pickle.load(f)
 
-    if isFast:
+    if is_fast:
         data['train'] = random.sample(data['train'], 20000)
         data['validate'] = random.sample(data['validate'], 100)
         data['test'] = random.sample(data['test'], 1000)
         max_epochs = 10
 
-    # organize data and translate from th to tf image ordering via .transpose( (0,2,3,1) )
-    X1_train = generate_imgs([x[0] for x in data['train']], font_location, font_size, image_size, text_location).transpose( (0,2,3,1) ) 
-    X2_train = generate_imgs([x[1] for x in data['train']], font_location, font_size, image_size, text_location).transpose( (0,2,3,1) )
-    y_train = [x[2] for x in data['train']]
+    # organize data and translate from th to tf image ordering via .transpose((0, 2, 3, 1))
+    X1_train = generate_imgs([x[0] for x in data['train']], font_location, font_size, image_size, text_location).transpose((0, 2, 3, 1))
+    X2_train = generate_imgs([x[1] for x in data['train']], font_location, font_size, image_size, text_location).transpose((0, 2, 3, 1))
+    y_train = [
+        x[2]
+        for x in data['train']
+    ]
 
-    X1_valid = generate_imgs([x[0] for x in data['validate']], font_location, font_size, image_size, text_location).transpose( (0,2,3,1) )
-    X2_valid = generate_imgs([x[1] for x in data['validate']], font_location, font_size, image_size, text_location).transpose( (0,2,3,1) )
-    y_valid = [x[2] for x in data['validate']]
+    X1_valid = generate_imgs([x[0] for x in data['validate']], font_location, font_size, image_size, text_location).transpose((0, 2, 3, 1))
+    X2_valid = generate_imgs([x[1] for x in data['validate']], font_location, font_size, image_size, text_location).transpose((0, 2, 3, 1))
+    y_valid = [
+        x[2]
+        for x in data['validate']
+    ]
 
-    X1_test = generate_imgs([x[0] for x in data['test']], font_location, font_size, image_size, text_location).transpose( (0,2,3,1) )
-    X2_test = generate_imgs([x[1] for x in data['test']], font_location, font_size, image_size, text_location).transpose( (0,2,3,1) )
-    y_test = [x[2] for x in data['test']]
+    X1_test = generate_imgs([x[0] for x in data['test']], font_location, font_size, image_size, text_location).transpose((0, 2, 3, 1))
+    X2_test = generate_imgs([x[1] for x in data['test']], font_location, font_size, image_size, text_location).transpose((0, 2, 3, 1))
+    y_test = [
+        x[2]
+        for x in data['test']
+    ]
 
     model = build_model((12, 150, 1))
 
@@ -165,13 +180,16 @@ if not os.path.isfile(OUTPUT_FILE):
     max_idx = 0
     for i in range(max_epochs):
         model.fit([X1_train, X2_train], y_train, batch_size=8, nb_epoch=1)
-        scores = [-x[0] for x in model.predict([X1_valid, X2_valid])]
+        scores = [
+            -x[0]
+            for x in model.predict([X1_valid, X2_valid])
+        ]
 
         t_auc = roc_auc_score(y_valid, scores)
         if t_auc > max_auc:
-            print('Updated best AUC from %f to %f' % (max_auc, t_auc))
+            print('Updated best AUC from {:f} to {:f}'.format(max_auc, t_auc))
             max_auc = t_auc
-            max_idx = i+1
+            max_idx = i + 1
 
     # Train on the correct number of epochs
     model = build_model((12, 150, 1))
@@ -183,41 +201,64 @@ if not os.path.isfile(OUTPUT_FILE):
     with open(os.path.join(OUTPUT_DIR, dataset_type + '_cnn.json'), 'wb') as f:
         f.write(json_string)
 
-    scores = [-x[0] for x in model.predict([X1_test, X2_test])]
+    scores = [
+        -x[0]
+        for x in model.predict([X1_test, X2_test])
+    ]
     fpr_siamese, tpr_siamese, _ = roc_curve(y_test, scores)
     roc_auc_siamese = auc(fpr_siamese, tpr_siamese)
 
-    #
     # Run Edit distance
-    #
-    scores = [(editdistance.eval(x[0].lower(), x[1].lower()), len(x[0]), 1.0-x[2]) for x in data['test']]
+    scores = [
+        (editdistance.eval(x[0].lower(), x[1].lower()), len(x[0]), 1.0 - x[2])
+        for x in data['test']
+    ]
 
-    y_percent_score = [float(x[0])/x[1] for x in scores]
+    y_percent_score = [
+        float(x[0]) / x[1]
+        for x in scores
+    ]
 
-    y_score, _, y_test = zip(*scores)
+    y_score, _, y_test = list(zip(*scores))
     fpr_ed, tpr_ed, _ = roc_curve(y_test, y_score)
     roc_auc_ed = auc(fpr_ed, tpr_ed)
 
     fpr_ps, tpr_ps, _ = roc_curve(y_test, y_percent_score)
     roc_auc_ps = auc(fpr_ps, tpr_ps)
 
-    #
     # Run editdistance visual similarity
-    # 
-    scores = [(strSimilarity.howConfusableAre(x[0].lower(), x[1].lower()), 1.0-x[2]) for x in data['test']]
+    scores = [
+        (strSimilarity.howConfusableAre(x[0].lower(), x[1].lower()), 1.0 - x[2])
+        for x in data['test']
+    ]
 
-    y_score, y_test = zip(*scores)
+    y_score, y_test = list(zip(*scores))
     fpr_edvs, tpr_edvs, _ = roc_curve(y_test, [-x for x in y_score])
     roc_auc_edvs = auc(fpr_edvs, tpr_edvs)
 
-    #
     # Store results
-    #
-    results = {}
-    results['editdistance_vs'] = {'fpr': fpr_edvs, 'tpr': tpr_edvs, 'auc':roc_auc_edvs}
-    results['editdistance'] = {'fpr': fpr_ed, 'tpr': tpr_ed, 'auc':roc_auc_ed}
-    results['editdistance_percent'] = {'fpr': fpr_ps, 'tpr': tpr_ps, 'auc':roc_auc_ps}
-    results['siamese'] = {'fpr': fpr_siamese, 'tpr': tpr_siamese, 'auc':roc_auc_siamese}
+    results = {
+        'siamese': {
+            'fpr': fpr_siamese,
+            'tpr': tpr_siamese,
+            'auc': roc_auc_siamese,
+        },
+        'editdistance_vs': {
+            'fpr': fpr_edvs,
+            'tpr': tpr_edvs,
+            'auc': roc_auc_edvs,
+        },
+        'editdistance': {
+            'fpr': fpr_ed,
+            'tpr': tpr_ed,
+            'auc': roc_auc_ed,
+        },
+        'editdistance_percent': {
+            'fpr': fpr_ps,
+            'tpr': tpr_ps,
+            'auc': roc_auc_ps,
+        },
+    }
 
     with open(OUTPUT_FILE, 'w') as f:
         pickle.dump(results, f)
